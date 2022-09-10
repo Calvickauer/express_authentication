655be641e8d3d11e9287bdd1f2b59efd621e704d
calvickauer <calvickauer@gmail.com>
2022-09-10 15:08:02 -0700

    crete env variable, add session and flash middleware

--- server.js
+++ server.js
@@ -17,6 +17,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
 
+app.use(session({
+  secret: SECRET_SESSION,    // What we actually will be giving the user on our site as a session cookie
+  resave: false,             // Save the session even if it's modified, make this false
+  saveUninitialized: true    // If we have a new session, we save it, therefore making that true
+}));
+app.use(flash());            // flash middleware
+
+app.use((req, res, next) => {
+  console.log('res locals.. >>> ',res.locals);
+  res.locals.alerts = req.flash();
+  res.locals.currentUser = req.user;
+  next();
+});
+
+
+
 app.get('/', (req, res) => {
   res.render('index');
 })
